c95a517315a207b5492dc1cc90bb87e6bc586e4c
d.dudkin <d.dudkin@growapp.biz>
2022-04-19 00:25:24 +0700

    remove storyboard

---
+++ Music/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  Music
+//
+//  Created by Дмитрий Дудкин on 19.04.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+}
