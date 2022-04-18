7dcb0669ea54ccc5990e0224baa50e2adff9ebc6
d.dudkin <d.dudkin@growapp.biz>
2022-04-19 01:06:01 +0700

    base cell and search bar

---
+++ Music/TrackModel.swift
@@ -0,0 +1,13 @@
+//
+//  TrackModel.swift
+//  Music
+//
+//  Created by Дмитрий Дудкин on 19.04.2022.
+//
+
+import Foundation
+
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
