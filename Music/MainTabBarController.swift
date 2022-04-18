b2dcc022419830d12f8bbb2b0ad7bc8fafac3bf9
d.dudkin <d.dudkin@growapp.biz>
2022-04-19 00:51:04 +0700

    tab bar base

--- Music/MainTabBarController.swift
+++ Music/MainTabBarController.swift
@@ -11,7 +11,28 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = .white
+        tabBar.tintColor = .systemPurple
         // Do any additional setup after loading the view.
+        
+        let searchViewController = SearchViewController()
+        let searchImage = UIImage(systemName: "magnifyingglass")
+        let libraryViewController = ViewController()
+        let libraryImage = UIImage(systemName: "music.note.house")
+        
+        viewControllers = [
+            createNavigationController(rootViewController: searchViewController, title: "Search", image: searchImage),
+            createNavigationController(rootViewController: libraryViewController, title: "Library", image: libraryImage)
+        ]
+    }
+    
+    func createNavigationController(rootViewController: UIViewController, title: String, image: UIImage?) -> UINavigationController{
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        navigationController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        
+        return navigationController
     }
 }
